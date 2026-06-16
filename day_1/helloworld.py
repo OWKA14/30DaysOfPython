9ee7ee9ffeba7117f37919f6ecf8f470c75e9d40
OWKA14 <owenkartika25@gmail.com>
2026-06-16 14:17:26 +0800

    day 1 challange

---
+++ day_1/helloworld.py
@@ -0,0 +1,26 @@
+# Day 1 - 30DaysOfPython Challange
+
+print(4 + 3)        # additon(+)
+print(4 - 3)        # subtraction(-)
+print(4 * 3)        # multiplication(*)
+print(3 / 4)        # division(/)
+print(3 ** 4)       # exponential(**)
+print(3 % 4)        # modulus(%)
+print(3 // 4)       # Floor division operator(//)
+print('Owen')
+print('Kartika')
+print('Indonesia')
+print('I am enjoying 30 days of python')
+
+# Checking data types
+print(type(10))     # Int
+print(type(9.8))
+print(type(3.14))   # Float
+print(type(4 + 4j)) # Complex number
+print(type('Asabeneh')) # String
+print(type([1, 2, 3]))  # List
+print(type({'name':'Asabeneh'}))    # Dictionary
+print(type({9.8, 3.14, 2.7}))       # Set
+print(type((9.8, 3.14, 2.7)))       # Tuple
+
+print(((10 - 2) ** 2 + (8 - 3) ** 2) ** 0.5) 
